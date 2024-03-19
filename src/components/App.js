fb11981ba87437b867ae21dd67032e5f012b06d1
Symeon Melnychuk <m.simeon998@gmail.com>
2024-03-19 20:30:40 +0200

    add bunner, section hero, section about

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
+import { AboutSection } from "./About/AboutSection";
 import css from "./App.module.css";
 
 import { HeaderSection } from "./Header/HeaderSection";
+import { HeroSection } from "./Hero/HeroSection";
 
 function GlobalWrapper() {
   return (
     <div className={css.App}>
       <div className={css.containerHero}>
         <HeaderSection />
+        <HeroSection />
       </div>
+      <AboutSection />
     </div>
   );
 }
--- src/components/Hero/HeroSection.js
+++ src/components/Hero/HeroSection.js
@@ -1,5 +1,25 @@
-import css from "./HeroSection.module.css"
+import css from "./HeroSection.module.css";
+import heroImage from "../../images/mobileImages/hero/hero_image@1x.png";
+import heroImage2x from "../../images/mobileImages/hero/hero_image@2x.png";
 
 export const HeroSection = () => {
-    return()
-}
+  return (
+    <div className={css.heroSection}>
+      <p className={css.textHero}>diD yOu seE iT ?</p>
+      <h1 className={css.heroTitle}>YACHT APES</h1>
+      <p className={css.textHero}>Apes aRe eveRywhere</p>
+      <picture>
+        <source
+          media="(max-width: 480px)"
+          srcSet={`${heroImage} 1x, ${heroImage2x} 2x`}
+        ></source>
+        <img src={heroImage} alt="Мавпа в окулярах"></img>
+      </picture>
+      <button className={css.buttonHero}>MEET APES</button>
+      <p className={css.heroLastText}>
+        YACHT APE IS A COLLECTION OF UNIQUE DIGITAL APES THAT YOU CAN OWN IN NFT
+        FORMAT
+      </p>
+    </div>
+  );
+};
